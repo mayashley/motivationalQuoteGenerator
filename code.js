0bcc7ec049577ca0b55ace59b5ae93bedf5870a5
mayashley <54293844+mayashley@users.noreply.github.com>
2019-09-23 13:27:36 -0400

    9/23/19
    established the base for quote generator and will continue to make progress tomorrow after work

---
+++ code.js
@@ -0,0 +1,9 @@
+// Function will pick a quote and corresponding author based on the random integer, then populate the quote in HTML
+function quote() {
+    // will store list of quotes
+    const quoteArray = [“The Way Get Started Is To Quit Talking And Begin Doing.”,“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.”,“Don’t Let Yesterday Take Up Too Much Of Today.”,“You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.”,“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”, “If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.”,“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.”,“We May Encounter Many Defeats But We Must Not Be Defeated.”,“Knowing Is Not Enough; We Must Apply. Wishing Is Not Enough; We Must Do.”,“We Generate Fears While We Sit. We Overcome Them By Action.”]
+    // will store the list of authors for quotes
+    const authorArray = ["Walt Disney","Winston Churchill","Will Rogers","Unknown","Vince Lombardi","Steve Jobs","Rob Siltanen","Maya Angelou","Johann Wolfgang Von Goethe","Dr. Henry Link"]
+
+    
+}
